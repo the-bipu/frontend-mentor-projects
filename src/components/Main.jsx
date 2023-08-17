d3c9d3bfebff2a2fed55701671b8928801e989b8
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-08-17 22:31:19 +0530

    Almost Done
    This challenge is almost done but links in some tags and some more modules are left.
    
    Date: 17 August 2023

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,72 @@
 import React from "react";
+import Footer from "./Footer";
 
 export default function Main () {
+
+    const [email, setEmail] = React.useState("");
+    const [isValidEmail, setIsValidEmail] = React.useState(true);
+
+    const [fname, setFname] = React.useState("");
+    const [isValidFname, setIsValidFname] = React.useState(true);
+
+    const [lname, setLname] = React.useState("");
+    const [isValidLname, setIsValidLname] = React.useState(true);
+
+    const [password, setPassword] = React.useState("");
+    const [isValidPassword, setIsValidPassword] = React.useState(true);
+
+    const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+
+    const handleEmailChange = (event) => {
+        const newEmail = event.target.value;
+        setEmail(newEmail);
+
+        if (newEmail.match(mailRegex)) {
+        setIsValidEmail(true);
+        } else {
+        setIsValidEmail(false);
+        }
+    };
+
+    const fnameChange = (event) => {
+        const newFname = event.target.value;
+        setFname(newFname);
+
+        if (newFname == ""){
+            setIsValidFname(false)
+        } else {
+            setIsValidFname(true)
+        }
+    }
+
+    const lnameChange = (event) => {
+        const newLname = event.target.value;
+        setLname(newLname);
+
+        if (newLname == ""){
+            setIsValidLname(false)
+        } else {
+            setIsValidLname(true)
+        }
+    }
+
+    const handlePasswordChange = (event) => {
+        const newPassword = event.target.value;
+        setPassword(newPassword);
+
+        if (newPassword == ""){
+            setIsValidPassword(false)
+        } else {
+            setIsValidPassword(true)
+        }
+    }
+
+    function MessageBox () {
+        if (fname!="" && lname!="" && email!="" && password!="") {
+            window.alert("Logged In!")
+        }
+    }
+
     return (
         <div className="container">
             <div className="con1-div1">
@@ -8,6 +74,7 @@ export default function Main () {
                 <p className="heading">Learn to code by watching others</p>
                 <p className="about">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
 
+                <Footer />
             </div>
             <div className="con1-div2">
 
@@ -17,26 +84,38 @@ export default function Main () {
                 <div className="con1-div2-2">
 
                     <div className="con1-div2-2-1 con1-iDiv-1">
-                        <input type="text" name="fname" placeholder="First Name" className="con2-Input" />
-                        <div className="warningDiv w-1">First Name cannot be empty</div>
+                        <input type="text" name="fname" placeholder="First Name" value={fname} 
+                        onChange={fnameChange} 
+                        className={`con2-Input border-style ${isValidFname ? "" : "wrongInput warningBorder"}`} />
+
+                        <div className={`warningDiv w-4 ${isValidFname ? "" : "wrongInput w-1-change"}`}>First Name cannot be empty</div>
                     </div>
 
                     <div className="con1-div2-2-1 con1-iDiv-2">
-                        <input type="text" name="lname" placeholder="Last Name" className="con2-Input" />
-                        <div className="warningDiv w-2">Last Name cannot be empty</div>
+                        <input type="text" name="lname" placeholder="Last Name" value={lname} 
+                        onChange={lnameChange} 
+                        className={`con2-Input border-style ${isValidLname ? "" : "wrongInput warningBorder"}`} />
+
+                        <div className={`warningDiv w-4 ${isValidLname ? "" : "wrongInput w-2-change"}`}>Last Name cannot be empty</div>
                     </div>
 
                     <div className="con1-div2-2-1 con1-iDiv-3">
-                        <input type="text" name="email" placeholder="Email Address" className="con2-Input" />
-                        <div className="warningDiv w-3">Looks like this is not an email</div>
+                        <input type="text" name="email" placeholder="Email Address" value={email} 
+                        onChange={handleEmailChange} 
+                        className={`con2-Input border-style ${isValidEmail ? "" : "wrongInput warningBorder"}`} />
+                        
+                        <div className={`warningDiv w-4 ${isValidEmail ? "" : "wrongInput w-3-change"}`}>Looks like this is not an email</div>
                     </div>
 
                     <div className="con1-div2-2-1 con1-iDiv-4">
-                        <input type="text" name="password" placeholder="Password" className="con2-Input" />
-                        <div className="warningDiv w-4">Password cannot be empty</div>
+                        <input type="text" name="password" placeholder="Password" value={password} 
+                        onChange={handlePasswordChange} 
+                        className={`con2-Input border-style ${isValidPassword ? "" : "wrongInput warningBorder"}`} />
+
+                        <div className={`warningDiv w-4 ${isValidPassword ? "" : "wrongInput w-3-change"}`}>Password cannot be empty</div>
                     </div>
 
-                    <button type="submit">Claim your free trial</button>
+                    <button type="submit" onClick={MessageBox}>Claim your free trial</button>
                     <p className="terms">By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
                 </div>
 
