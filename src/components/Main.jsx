8b2ead5dcd48a3c0453bef4710e9aa8fe43c7fa2
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-08-17 09:12:36 +0530

    Changes in Style
    Added new components and changes in the style of form. Also added the verification message and warning logo for the verification of form.
    
    The verification part and mobile design is remaining in this project.
    
    Date: 17 August 2023

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,10 +15,26 @@ export default function Main () {
                     <p className="plans">Try it free 7 days <span>then $20/mo. thereafter</span></p>
                 </div>
                 <div className="con1-div2-2">
-                    <input type="text" placeholder="First Name" />
-                    <input type="text" placeholder="Last Name" />
-                    <input type="text" placeholder="Email Address" />
-                    <input type="text" placeholder="Password" />
+
+                    <div className="con1-div2-2-1 con1-iDiv-1">
+                        <input type="text" name="fname" placeholder="First Name" className="con2-Input" />
+                        <div className="warningDiv w-1">First Name cannot be empty</div>
+                    </div>
+
+                    <div className="con1-div2-2-1 con1-iDiv-2">
+                        <input type="text" name="lname" placeholder="Last Name" className="con2-Input" />
+                        <div className="warningDiv w-2">Last Name cannot be empty</div>
+                    </div>
+
+                    <div className="con1-div2-2-1 con1-iDiv-3">
+                        <input type="text" name="email" placeholder="Email Address" className="con2-Input" />
+                        <div className="warningDiv w-3">Looks like this is not an email</div>
+                    </div>
+
+                    <div className="con1-div2-2-1 con1-iDiv-4">
+                        <input type="text" name="password" placeholder="Password" className="con2-Input" />
+                        <div className="warningDiv w-4">Password cannot be empty</div>
+                    </div>
 
                     <button type="submit">Claim your free trial</button>
                     <p className="terms">By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
