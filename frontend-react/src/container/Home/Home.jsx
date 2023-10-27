a2d47bb0bc067e316645c25342f291599c406099
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-27 13:42:57 +0530

    Update Home.jsx

--- frontend-react/src/container/Home/Home.jsx
+++ frontend-react/src/container/Home/Home.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 
 import './Home.scss';
+import images from '../../constants/images';
 
 const Home = () => {
   return (
-    <div>
-      <div class="main__div-nav">
-        Home
-        Shop
-        About
-        Contact
+    <div className='main__div'>
+      <div className="main__div-nav">
+          <div className="nav">
+            <div className="nav-head">
+              Room
+            </div>
+            <div className="nav-itemCon">
+              <div className="nav-items">Home</div>
+              <div className="nav-items">Shop</div>
+              <div className="nav-items">About</div>
+              <div className="nav-items">Contact</div>
+            </div>
+          </div>
       </div>
 
-      <div class="main__div-content">
-        Discover innovative ways to decorate
+      <div className="main__div-content">
+        <div className="main__div-content-heading">
+          Discover innovative ways to decorate
+        </div>
+        <div className="main__div-content-para">
+          We provide unmatched quality, comfort, and style for property owners across the country. 
+          Our experts combine form and function in bringing your vision to life. Create a room in your 
+          own style with our collection and make your property a reflection of you and what you love.
+        </div>
+        <button className='shopNow__button'>
+          Shop now
+        </button>
 
-        We provide unmatched quality, comfort, and style for property owners across the country. 
-        Our experts combine form and function in bringing your vision to life. Create a room in your 
-        own style with our collection and make your property a reflection of you and what you love.
-      
-        Shop now
+        <div className="arrowDiv">
+          <div className="firstArrow">
+            <img src="" alt="" />
+          </div>
+          <div className="secondArrow">
+            <img src="" alt="" />
+          </div>
+        </div>
       </div>
 
-      <div class="main__div-content">
+      {/* <div class="main__div-content">
         We are available all across the globe
 
         With stores all over the world, it's easy for you to find furniture for your home or place of business. 
@@ -40,24 +61,31 @@ const Home = () => {
         experience in this industry, we understand what customers want for their home and office.
 
         Shop now
+      </div> */}
+
+      <div className="mini__div-image1">
+        <img src={images.aboutDark} alt="" />
       </div>
 
-      <div class="mini__div-image1"></div>
-
-      <div class="mini__div-content">
-        About our furniture
-
-        Our multifunctional collection blends design and function to suit your individual taste.
-        Make each room unique, or pick a cohesive theme that best express your interests and what
-        inspires you. Find the furniture pieces you need, from traditional to contemporary styles
-        or anything in between. Product specialists are available to help you create your dream space.
+      <div className="mini__div-content">
+        <div className="mini__div-content-heading">
+          About our furniture
+        </div>
+        <div className="mini__div-content-para">
+          Our multifunctional collection blends design and function to suit your individual taste.
+          Make each room unique, or pick a cohesive theme that best express your interests and what
+          inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+          or anything in between. Product specialists are available to help you create your dream space.
+        </div>
       </div>
       
-      <div class="mini__div-image2"></div>
+      <div className="mini__div-image2">
+        <img src={images.aboutLight} alt="" />
+      </div>
       
-      <div class="attribution">
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="#">The Bipu</a>.
       </div>
     </div>
   )
