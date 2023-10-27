83807a5d61053660cf569fc4c558e82276010a8c
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-27 13:42:33 +0530

    Create images.js

---
+++ frontend-react/src/constants/images.js
@@ -0,0 +1,22 @@
+import desktopHeroI1 from '../assets/images/desktop-image-hero-1.jpg';
+import desktopHeroI2 from '../assets/images/desktop-image-hero-2.jpg';
+import desktopHeroI3 from '../assets/images/desktop-image-hero-3.jpg';
+
+import aboutDark from '../assets/images/image-about-dark.jpg';
+import aboutLight from '../assets/images/image-about-light.jpg';
+
+import mobileHeroI1 from '../assets/images/mobile-image-hero-1.jpg';
+import mobileHeroI2 from '../assets/images/mobile-image-hero-2.jpg';
+import mobileHeroI3 from '../assets/images/mobile-image-hero-3.jpg';
+
+// eslint-disable-next-line
+export default {
+    desktopHeroI1,
+    desktopHeroI2,
+    desktopHeroI3,
+    aboutDark,
+    aboutLight,
+    mobileHeroI1,
+    mobileHeroI2,
+    mobileHeroI3,
+};
