5c536f87188addb3063408941dbcbab36ba2ae86
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-28 16:50:39 +0530

    Update images.js

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -9,6 +9,15 @@ import mobileHeroI1 from '../assets/images/mobile-image-hero-1.jpg';
 import mobileHeroI2 from '../assets/images/mobile-image-hero-2.jpg';
 import mobileHeroI3 from '../assets/images/mobile-image-hero-3.jpg';
 
+import iconArrow from '../assets/images/icon-arrow.svg';
+import iconClose from '../assets/images/icon-close.svg';
+import iconHamburger from '../assets/images/icon-hamburger.svg';
+
+import iconLeft from '../assets/images/icon-angle-left.svg';
+import iconRight from '../assets/images/icon-angle-right.svg';
+
+import logo from '../assets/images/logo.svg';
+
 // eslint-disable-next-line
 export default {
     desktopHeroI1,
@@ -19,4 +28,10 @@ export default {
     mobileHeroI1,
     mobileHeroI2,
     mobileHeroI3,
+    iconArrow,
+    iconClose,
+    iconHamburger,
+    iconLeft,
+    iconRight,
+    logo,
 };
