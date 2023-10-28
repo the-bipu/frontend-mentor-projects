1a0159a7df3837a7a590339f75acfd6c4d3beae8
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-28 22:56:13 +0530

    Create App.js

---
+++ roomhomestore/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Home } from './container';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
