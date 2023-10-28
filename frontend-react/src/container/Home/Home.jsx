41c6b19841291494a8f40f16c57222087d411823
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-28 16:50:44 +0530

    Update Home.jsx

--- frontend-react/src/container/Home/Home.jsx
+++ frontend-react/src/container/Home/Home.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Home.scss';
 import images from '../../constants/images';
 
 const Home = () => {
+  const [selectedBackground, setSelectedBackground] = useState(0);
+  const [selectedContent, setSelectedContent] = useState(0);
+
+  const backgroundOptions = ['url(../../assets/images/desktop-image-hero-1.jpg)', images.desktopHeroI2, images.desktopHeroI3];
+  // const backgroundOptions = [
+  //   'pink',
+  //   'red', 
+  //   'orange'
+  // ];
+
+  const contentOptions = [
+    { heading: "Discover innovative ways to decorate", para: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." },
+    { heading: "We are available all across the globe", para: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."},
+    { heading: "Manufactured with the best materials", para: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."}
+  ];
+
+  const changeLeft = () => {
+    console.log("1st background changed !!")
+    setSelectedBackground((prevBackground) => (prevBackground - 1 + backgroundOptions.length) % backgroundOptions.length);
+    setSelectedContent((prevContent) => (prevContent - 1 + contentOptions.length) % contentOptions.length);
+  };
+
+  const changeRight = () => {
+    console.log("2nd background changed !!")
+    setSelectedBackground((prevBackground) => (prevBackground + 1) % backgroundOptions.length);
+    setSelectedContent((prevContent) => (prevContent + 1) % contentOptions.length);
+  };
+
+  const currentContent = contentOptions[selectedContent];
+
   return (
     <div className='main__div'>
-      <div className="main__div-nav">
+      <div className="main__div-nav" style={{ background: backgroundOptions[selectedBackground] }}>
           <div className="nav">
             <div className="nav-head">
               Room
@@ -21,25 +51,27 @@ const Home = () => {
       </div>
 
       <div className="main__div-content">
-        <div className="main__div-content-heading">
-          Discover innovative ways to decorate
+        <div className="main__div-contentCons">
+          <div className="main__div-content-heading">
+            {currentContent.heading}
+          </div>
+          <div className="main__div-content-para">
+            {currentContent.para}
+          </div>
+          <button className='shopNow__button'>
+            Shop now
+            &nbsp;
+            <img src={images.iconArrow} alt="" />
+          </button>
         </div>
-        <div className="main__div-content-para">
-          We provide unmatched quality, comfort, and style for property owners across the country. 
-          Our experts combine form and function in bringing your vision to life. Create a room in your 
-          own style with our collection and make your property a reflection of you and what you love.
-        </div>
-        <button className='shopNow__button'>
-          Shop now
-        </button>
 
         <div className="arrowDiv">
-          <div className="firstArrow">
-            <img src="" alt="" />
-          </div>
-          <div className="secondArrow">
-            <img src="" alt="" />
-          </div>
+          <button onClick={changeLeft} className="firstArrow">
+            <img src={images.iconLeft} alt="" />
+          </button>
+          <button onClick={changeRight} className="secondArrow">
+            <img src={images.iconRight} alt="" />
+          </button>
         </div>
       </div>
 
