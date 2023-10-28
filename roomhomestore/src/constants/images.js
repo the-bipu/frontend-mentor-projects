6171be8b98e300af408a6d77e7c288d1646e84d7
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-28 22:57:43 +0530

    Create images.js
    export all images as constants

---
+++ roomhomestore/src/constants/images.js
@@ -0,0 +1,37 @@
+import desktopHeroI1 from '../assets/desktop-image-hero-1.jpg';
+import desktopHeroI2 from '../assets/desktop-image-hero-2.jpg';
+import desktopHeroI3 from '../assets/desktop-image-hero-3.jpg';
+
+import aboutDark from '../assets/image-about-dark.jpg';
+import aboutLight from '../assets/image-about-light.jpg';
+
+import mobileHeroI1 from '../assets/mobile-image-hero-1.jpg';
+import mobileHeroI2 from '../assets/mobile-image-hero-2.jpg';
+import mobileHeroI3 from '../assets/mobile-image-hero-3.jpg';
+
+import iconArrow from '../assets/icon-arrow.svg';
+import iconClose from '../assets/icon-close.svg';
+import iconHamburger from '../assets/icon-hamburger.svg';
+
+import iconLeft from '../assets/icon-angle-left.svg';
+import iconRight from '../assets/icon-angle-right.svg';
+
+import logo from '../assets/logo.svg';
+
+// eslint-disable-next-line
+export default {
+    desktopHeroI1,
+    desktopHeroI2,
+    desktopHeroI3,
+    aboutDark,
+    aboutLight,
+    mobileHeroI1,
+    mobileHeroI2,
+    mobileHeroI3,
+    iconArrow,
+    iconClose,
+    iconHamburger,
+    iconLeft,
+    iconRight,
+    logo,
+};
