2fb1c545b731d571634aa0771bbb73b7864be910
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-16 00:29:31 +0530

    update homepage

--- Room_HomePage/src/pages/Home/Home.jsx
+++ Room_HomePage/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import images from '../../constants/images';
 const Home = () => {
   const [selectedBackground, setSelectedBackground] = useState(0);
   const [selectedContent, setSelectedContent] = useState(0);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const backgroundOptions = [
     `url(${images.desktopHeroI1})`,
@@ -34,87 +35,12 @@ const Home = () => {
   const currentContent = contentOptions[selectedContent];
 
   return (
-    // <div className='flex flex-col w-full min-h-screen'>
-
-    //   <div className='flex flex-row w-full h-[60%]'>
-
-    //     <div className="main__div-nav w-[58%]" style={{ background: backgroundOptions[selectedBackground] }}>
-    //       <div className="nav">
-    //         <div className="nav-head">
-    //           <img src={images.logo} alt="" />
-    //         </div>
-    //         <div className="nav-itemCon">
-    //           <div className="nav-items">Home</div>
-    //           <div className="nav-items">Shop</div>
-    //           <div className="nav-items">About</div>
-    //           <div className="nav-items">Contact</div>
-    //         </div>
-    //       </div>
-    //     </div>
-
-    //     <div className="main__div-content w-[40%]">
-
-    //       <div className={`main__div-contentCons relative`}>
-
-    //         <div className="main__div-content-heading">
-    //           {currentContent.heading}
-    //         </div>
-    //         <div className="main__div-content-para">
-    //           {currentContent.para}
-    //         </div>
-    //         <button className='shopNow__button flex flex-row gap-3'>
-    //           Shop now
-    //           &nbsp;
-    //           <img src={images.iconArrow} alt="" />
-    //         </button>
-
-    //         <div className="arrowDiv absolute bottom-0 left-0">
-    //           <button onClick={changeLeft} className="firstArrow">
-    //             <img src={images.iconLeft} alt="" />
-    //           </button>
-    //           <button onClick={changeRight} className="secondArrow">
-    //             <img src={images.iconRight} alt="" />
-    //           </button>
-    //         </div>
-
-    //       </div>
-
-    //     </div>
-
-    //   </div>
-
-    //   <div className='flex flex-row w-full'>
-
-    //     <div className='bg-aboutDark w-[32%] h-full'></div>
-
-    //     <div className="mini__div-content w-[33%]">
-    //       <div className="mini__div-content-heading">
-    //         About our furniture
-    //       </div>
-    //       <div className="mini__div-content-para">
-    //         Our multifunctional collection blends design and function to suit your individual taste.
-    //         Make each room unique, or pick a cohesive theme that best express your interests and what
-    //         inspires you. Find the furniture pieces you need, from traditional to contemporary styles
-    //         or anything in between. Product specialists are available to help you create your dream space.
-    //       </div>
-    //     </div>
-
-    //     <div className='bg-aboutLight w-[32%] h-full'></div>
-
-    //   </div>
-
-    //   <div className="attribution">
-    //     Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-    //     Coded by <a href="https://github.com/the-bipu">The Bipu</a>.
-    //   </div>
-    // </div>
-
     <div className='w-full h-full min-h-screen bg-white flex flex-col'>
 
       <div className='flex md:flex-row flex-col w-full'>
 
         <div
-          className="w-[60%] min-h-[500px]"
+          className="md:w-[60%] w-full min-h-[500px]"
           style={{
             backgroundImage: backgroundOptions[selectedBackground],
             backgroundSize: 'cover',
@@ -122,20 +48,35 @@ const Home = () => {
             backgroundPosition: 'center',
           }}
         >
-          <div className="navbar flex items-center">
-            <div className="navbar-head">
-              <img src={images.logo} alt="" />
+          <div className={`navbar w-full flex items-center md:justify-start justify-center flex-row lowercase gap-9 text-white md:py-12 py-8 md:pl-16 pl-8 relative ${drawerOpen && 'bg-white w-full h-20'}`}>
+
+            <div className='md:hidden flex absolute left-8 top-8'>
+              <img src={images.iconHamburger} alt="" onClick={() => setDrawerOpen(!drawerOpen)} className={`${drawerOpen ? 'hidden' : ''}`} />
+              <img src={images.iconClose} alt="" onClick={() => setDrawerOpen(!drawerOpen)} className={`${drawerOpen ? '' : 'hidden'}`} />
             </div>
-            <div className="flex flex-row gap-4 nav-items">
+
+            <div className={`flex gap-3 text-xl justify-center flex-row text-black ${drawerOpen ? 'flex' : 'hidden'}`}>
               <div className="nav-item">Home</div>
               <div className="nav-item">Shop</div>
               <div className="nav-item">About</div>
               <div className="nav-item">Contact</div>
             </div>
+
+            <div className={`navbar-head ${drawerOpen && 'hidden'}`}>
+              <img src={images.logo} alt="" />
+            </div>
+
+            <div className="md:flex hidden flex-row gap-7 justify-center items-center text-xl nav-items">
+              <div className="nav-item">Home</div>
+              <div className="nav-item">Shop</div>
+              <div className="nav-item">About</div>
+              <div className="nav-item">Contact</div>
+            </div>
+
           </div>
         </div>
 
-        <div className='w-[40%] py-32 px-24 relative'>
+        <div className='md:w-[40%] w-full md:py-32 py-20 md:px-24 px-10 relative'>
 
           <div className={`w-full h-auto`}>
 
@@ -151,7 +92,16 @@ const Home = () => {
               <img src={images.iconArrow} alt="" />
             </button>
 
-            <div className="absolute bottom-0 left-0 arrowDiv">
+            <div className="absolute bottom-0 left-0 w-40 h-20 md:flex flex-row bg-black arrowDiv">
+              <button onClick={changeLeft} className="firstArrow">
+                <img src={images.iconLeft} alt="" />
+              </button>
+              <button onClick={changeRight} className="secondArrow">
+                <img src={images.iconRight} alt="" />
+              </button>
+            </div>
+
+            <div className="md:hidden flex absolute -top-20 right-0 arrowDiv">
               <button onClick={changeLeft} className="firstArrow">
                 <img src={images.iconLeft} alt="" />
               </button>
@@ -165,8 +115,30 @@ const Home = () => {
 
       </div>
 
-      <div className='flex md:flex-row flex-col w-full bg-yellow-300'></div>
+      <div className='flex md:flex-row flex-col w-full'>
+
+        <div className='bg-aboutDark min-w-[30%] h-full'></div>
+
+        <div className="w-auto p-12 flex flex-col items-start justify-center">
+          <div className="text-lg font-bold uppercase mb-7 text-black">
+            About our furniture
+          </div>
+          <div className="text-base leading-5 text-[#a1a1a1]">
+            Our multifunctional collection blends design and function to suit your individual taste.
+            Make each room unique, or pick a cohesive theme that best express your interests and what
+            inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+            or anything in between. Product specialists are available to help you create your dream space.
+          </div>
+        </div>
+
+        <div className='bg-aboutLight min-w-[30%] h-full'></div>
+      </div>
     </div>
+
+    //   <div className="attribution">
+    //     Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+    //     Coded by <a href="https://github.com/the-bipu">The Bipu</a>.
+    //   </div>
   )
 }
 
